0b524b2b391b36515404dcd390f74883947e16ab
Randy Rivera <rivera.na2015@gmail.com>
2022-10-03 09:16:04 -0400

    created three simple functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+    console.log("Name: Randy Rivera")
+    console.log("Favorite Movie: Star Wars")
+    console.log("Favorite Band: Twenty One Pilots")
+}
+
+function whyImHere() {
+    console.log("I joined this Learning to Code program because I know I can become/do better. I want to make myself and my family proud. It's an amazing way to prove to everyone including myself that I'm more than capable.")
+}
+
+function favoritePlace() {
+    console.log("One of my favorite places to visit would be Iceland or Italy. Since I'm a huge video game fanatic, both of my favorite games are based off those locations.")
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
