2b6897627dacd8234bd28bff768cfc3315da16a0
Randy Rivera <rivera.na2015@gmail.com>
2022-10-03 11:40:56 -0400

    created another function with if / else if

--- Functions/tax_functions.js
+++ Functions/tax_functions.js
@@ -2,15 +2,31 @@ function getSocSecTax(amount) {
     return amount * (6.2 / 100);
 }
 
-let grossPay = 50000;
+let grossPay = 1042;
 let socialTax = getSocSecTax(grossPay);
-console.log(socialTax);
+console.log(socialTax.toFixed(2));
 
 function getMedicareTax(amount) {
     return amount *  (1.45 / 100);
 }
 
-let grossAmount = 50000;
+let grossAmount = 1042;
 let medicareTax = getMedicareTax(grossAmount);
-console.log(medicareTax);
-// if they wanted the monthly amount I would just divide medicareTax / 12 or socialTax / 12;
+console.log(medicareTax.toFixed(2));
+
+function getFederalTax(amount, code) {
+    if (code == 0) {
+        return amount * (23 / 100)
+    } else if (code == 1) {
+        return amount * (21 / 100)
+    } else if (code == 2) {
+        return amount * (19.5 / 100) 
+    } else if (code == 3) {
+        return amount * (18.5 / 100)
+    } else {
+        return amount * .18 - (2 * .005)
+    }
+}
+
+
+console.log(getFederalTax(1042, 2))
