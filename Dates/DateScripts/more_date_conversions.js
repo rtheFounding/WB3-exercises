a5beb4a6d9c5b793a1fe63c9134469fb73474fe5
Randy Rivera <rivera.na2015@gmail.com>
2022-10-04 11:09:09 -0400

    Date objects, displaying with toLocaleString

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,7 @@
+let myBirthday = new Date("04/27/1997");
+let momsBirthday = new Date("Dec 01 1969");
+let dadsBirthday = new Date(1967, 4, 9);
+
+console.log(myBirthday.toLocaleString());
+console.log(momsBirthday.toLocaleString());
+console.log(dadsBirthday.toLocaleString());
