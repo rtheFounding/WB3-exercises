8eb310a277b3c75d63166cf9cf68e0c749ec46f3
Randy Rivera <rivera.na2015@gmail.com>
2022-10-04 10:56:11 -0400

    grabs current date/time, displays using methods

---
+++ Dates/DateScripts/date_to_string.js
@@ -0,0 +1,7 @@
+let currentDateATime = new Date();
+console.log(currentDateATime)
+
+console.log(currentDateATime.toString());
+console.log(currentDateATime.toDateString());
+console.log(currentDateATime.toUTCString());
+console.log(currentDateATime.toLocaleString());
